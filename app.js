712d27a236acef058edb722c298cbd2dde766248
mufidu <mufidu@outlook.com>
2022-04-26 17:25:12 +0700

    Seventh attempt to deploy

--- app.js
+++ app.js
@@ -78,6 +78,6 @@ app.delete('/books/:id', async (req, res) => {
 
 const port = process.env.PORT || 8080;
 
-app.listen(8080, () => {
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
